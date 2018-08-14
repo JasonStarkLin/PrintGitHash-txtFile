bed17399d525265d93a8ed894c8b737efc688b2a
JasonStarkLin <42349638+JasonStarkLin@users.noreply.github.com>
2018-08-15 03:45:07 +0800

    THe main start
    Main start

---
+++ PrintGitHash.py
@@ -0,0 +1,10 @@
+AnaFile=open('Analysis.txt','w',newline=None)
+
+AnaFile.write("Today is 8/15 please check it.\n")
+AnaFile.write("This is a new line\n")
+AnaFile.write("Hellow world\n")
+AnaFile.write("Try your best to learn python\n")
+
+AnaFile.close
+
+print('End of program')
